153766ca2fdef9804533653df9e2c8eb1438151a
MarcelinoVinicius <vinimarces89@gmail.com>
2025-06-03 21:40:13 -0300

    primeira aula de JS introdução

---
+++ JS/JS01-Introducao/main.js
@@ -0,0 +1,43 @@
+//
+
+/* 
+
+
+
+*/
+
+// let não pode ter duas variaveis com o mesmo nome em um documento
+// const deixa o valor fixo no codigo
+
+let idade 
+const ano = 2010
+
+console.log("Hello word") // apresentar mesagem
+console.clear() // limpar
+
+// variaveis
+
+var numero1 = 192 // iniciar uma variavel
+console.log(numero1) 
+
+var num1 = 4
+var num2 = 4
+var resultado = num1 + num2
+console.log(resultado)
+
+idade = 89 // iniciar a variavel em outra parte do codigo
+console.log(idade)
+
+ano = 1936
+
+//textos strings
+var nome = 'Matheus Araujo'
+var nome2 = 'Matheus Vinicius'
+var nome3 = '12345.6789' // texto
+console.log(nome, nome2, nome3)
+
+// var é flexivl e com menos regras (posso alterar o valor inicial de uma var e recriar a mesma em outras partes do codigo)
+
+// let você não pode recriar a mesma em outras partes do codigo com o mesmo nome, mas pode chamar e alterar o valor inicial
+
+// const você não pode recriar a mesma em outras partes do codigo e não pode alterar  o valor inicial dela garantindo a confiança do dado que está sendo utiliado no decorrer do seu codigo
