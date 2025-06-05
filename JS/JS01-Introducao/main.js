44fdabe29622621deb7b65efd44ce2b7460c5a8b
MarcelinoVinicius <vinimarces89@gmail.com>
2025-06-04 21:34:55 -0300

    atualização da aula 01 de JS introdução

--- JS/JS01-Introducao/main.js
+++ JS/JS01-Introducao/main.js
@@ -28,16 +28,56 @@ console.log(resultado)
 idade = 89 // iniciar a variavel em outra parte do codigo
 console.log(idade)
 
-ano = 1936
-
 //textos strings
 var nome = 'Matheus Araujo'
 var nome2 = 'Matheus Vinicius'
 var nome3 = '12345.6789' // texto
 console.log(nome, nome2, nome3)
 
+console.log("Olá" + " papai") // concatenar
+
+console.log("Aluno " + nome + "e Aluno " + nome2 + 'Tem um total de ' + nome3 + 'dinheiros') // concatenar
+
+//template string
+
+console.log(`Aluno ${nome} e Aluno ${nome2} Tem um total de ${nome3} dinheiros`) //concatenar
+
 // var é flexivl e com menos regras (posso alterar o valor inicial de uma var e recriar a mesma em outras partes do codigo)
 
 // let você não pode recriar a mesma em outras partes do codigo com o mesmo nome, mas pode chamar e alterar o valor inicial
 
 // const você não pode recriar a mesma em outras partes do codigo e não pode alterar  o valor inicial dela garantindo a confiança do dado que está sendo utiliado no decorrer do seu codigo
+
+
+// Operadores
+
+var valor1 = 5
+var valor2 = 5
+console.log(valor2 + valor1)
+console.log(valor1 - valor2)
+console.log(valor1 * valor2)
+console.log(valor1 / valor2)
+console.log(valor1 ** valor2)
+
+console.log(5 / 2 * (2 + 3))
+
+//Operadores relacionais
+
+console.log(5 == 5)
+console.log(5 > 5)
+console.log(22 < 8)
+console.log(22 >= 22)
+console.log(22 != 23)
+console.log('A' == 'A')
+console.log(true == false)
+console.log(true == true)
+console.log(true != 'false')
+
+let temCamisa = false
+let temCracha = true
+console.log(temCamisa)
+console.log(temCracha)
+console.log(temCamisa == true && temCracha == true) // && = "and" ou "e"
+console.log(temCamisa == true || temCracha == true) // || = "our" ou "ou"
+console.log(!(temCamisa == true || temCracha == true))
+
