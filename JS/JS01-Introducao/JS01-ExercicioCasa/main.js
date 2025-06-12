221e9f87e4d24f69e5b2306b5dbdda9f6e14dc28
MarcelinoVinicius <vinimarces89@gmail.com>
2025-06-12 19:16:52 -0300

    Envio do excercio de casa de JS da aula 01

---
+++ JS/JS01-Introducao/JS01-ExercicioCasa/main.js
@@ -0,0 +1,9 @@
+var nota01 = 07
+var nota02 = 08
+var nota03 = 10
+var nota04 = 07
+var resultadoTotal = nota01 + nota02 + nota03 + nota04
+var resultadoMedia = resultadoTotal / 4
+var texto = "A média do aluno com as 4 notas é: "
+console.log(texto + resultadoMedia)
+
