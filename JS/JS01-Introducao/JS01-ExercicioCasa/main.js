c7019948289695d10d88f4e077a08e9e2e0a1887
MarcelinoVinicius <vinimarces89@gmail.com>
2025-06-12 20:05:29 -0300

    subida de excercicio da aula 03 condicional de JS

--- JS/JS01-Introducao/JS01-ExercicioCasa/main.js
+++ JS/JS01-Introducao/JS01-ExercicioCasa/main.js
@@ -1,7 +1,7 @@
-var nota01 = 07
-var nota02 = 08
+var nota01 = 7
+var nota02 = 8
 var nota03 = 10
-var nota04 = 07
+var nota04 = 7
 var resultadoTotal = nota01 + nota02 + nota03 + nota04
 var resultadoMedia = resultadoTotal / 4
 var texto = "A média do aluno com as 4 notas é: "
---
+++ JS/JS03-Condicional/mainExcercicio.js
@@ -0,0 +1,10 @@
+var numero1 = 15
+var numero2 = 8
+
+if (numero1 > numero2){
+    console.log('numero1 é maior que numero2')
+}
+else if ( numero1 < numero2) {
+    console.log('numero1 é menor que numero2');
+}
+
