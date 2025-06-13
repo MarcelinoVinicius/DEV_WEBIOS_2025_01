53699f66ffe0aa09d969ba9a7c3286b7f7ab800d
MarcelinoVinicius <vinimarces89@gmail.com>
2025-06-12 21:03:03 -0300

    Subida da aula 04 de JS repetição e Excercicio

---
+++ JS/JS04-Repeticao/main.js
@@ -0,0 +1,20 @@
+let contador = 0
+
+// enquanto a variavel contador for menor que 10 o while vai apresentar o valor davariavel no console
+
+while (contador < 10) {
+    console.log(contador)
+    // contador += 1
+    // contador = contador + 1
+    contador++
+}
+console.log('Finalizou o programa');
+
+for (contador = 0; contador <= 10; contador++){
+    console.log(contador);
+}
+
+
+
+
+
