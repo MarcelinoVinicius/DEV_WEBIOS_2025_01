2870f65125b3c01cb644d5eea6136ad9ece39034
MarcelinoVinicius <vinimarces89@gmail.com>
2025-06-27 15:30:24 -0300

    atualização

--- TESTES/ESTUDOS-02/JS/main.js
+++ TESTES/ESTUDOS-02/JS/main.js
@@ -1,4 +1,4 @@
-function toggleMenu() {
+  function toggleMenu() {
     const menu = document.getElementById("menu-items");
     menu.style.display = menu.style.display === "block" ? "none" : "block";
   }
