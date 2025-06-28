2d44a15bd59869f74f9913d6a8bf6a4789aae91b
MarcelinoVinicius <vinimarces89@gmail.com>
2025-06-28 12:00:12 -0300

    atualização nova

--- TESTES/ESTUDOS-02/JS/main.js
+++ TESTES/ESTUDOS-02/JS/main.js
@@ -2,3 +2,13 @@
     const menu = document.getElementById("menu-items");
     menu.style.display = menu.style.display === "block" ? "none" : "block";
   }
+
+  const modal = document.querySelector('.modal-container')
+
+function openModal() {
+  modal.classList.add('active')
+}
+
+function closeModal() {
+  modal.classList.remove('active')
+}
